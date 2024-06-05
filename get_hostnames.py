2aed9890bee2a2d8c05d8026476caf3c3069f2b1
longtv119 <104315061+GnolV@users.noreply.github.com>
2024-06-05 15:05:06 +0700

    Update get_hostnames.py

--- get_hostnames.py
+++ get_hostnames.py
@@ -6,39 +6,65 @@ import ipaddress
 from concurrent.futures import ThreadPoolExecutor
 from bs4 import BeautifulSoup
 from time import sleep
+from random import randint
 
-'''https://ipinfo.io/countries/vn#section-routers'''
-
-headers = {
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0"
-}
-
-'''Lấy danh sách các ASN ở Việt Nam'''
-def get_asn_list_vn():
-    res = requests.get(url="https://whoisrequest.com/ip/vn", headers=headers)
+'''Lấy số lượng ASN ở Việt Nam'''
+def get_asn_amount():
+    url = "https://ipinfo.io/countries/vn"
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0"
+        }
+    res = requests.get(url=url, headers=headers)
     soup = BeautifulSoup(res.text, "lxml")
-    table = soup.find_all("tr")
-    asn_vn_list = []
-    for datas in table:
-        data = datas.find_all("td")
-        if len(data) == 3  and data[1].text.strip() != '':
-            asn_vn_list.append({
-                "asn": data[0].text.strip(),
-                "organization": data[1].text.strip(),
-                "ips_assigned": data[2].text.strip()
-            })
+    rows = soup.find_all("tr", class_="even:bg-charcoal-blue-03 px-6")[0]
+    data = rows.find("a")
+    # print(data.text.strip())
+    return data.text.strip()
 
-    '''Lưu danh sách vào file csv'''
-    with open("DS_ASN_VN.csv", "w", newline="") as csvfile:
-        writer = csv.writer(csvfile)
-        fields = asn_vn_list[0].keys()
-        writer.writerow(fields)
-        for item in asn_vn_list:
-            writer.writerow(item.values())
+'''Xuất danh sách các ASN ở Việt Nam'''
+def export_asn_list_vn():
+    url = "https://ipinfo.io/api/data/asns?country=vn&amount=20&page={}"
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0"
+        }
+    amount = int(get_asn_amount())
+    with open("DS_ASN_VN.csv", "w", newline="") as file:
+        writer = csv.writer(file)
+        header = ["ASN","Name", "Country", "Number of IPs", "Type"]
+        writer.writerow(header)
+        for i in range(int(amount/20)+1):
+            # sleep(randint(1, 5))
+            res = requests.get(url=url.format(i), headers=headers)
+            data = res.json()
+            if isinstance(data, list):
+                for item in data:
+                    writer.writerow(list(item.values()))
 
+'''Cập nhật danh sách ASN ở Việt Nam ra file csv'''
+def update_asn_list():
+    amount = int(get_asn_amount())
+    with open("DS_ASN_VN.csv", "r") as file:
+        reader = csv.reader(file)
+        cnt = sum(1 for _ in reader)
+    if cnt < amount:
+        with open("DS_ASN_VN.csv", "w", newline="") as file:
+            writer = csv.writer(file)
+            header = ["ASN","Name", "Country", "Number of IPs", "Type"]
+            writer.writerow(header)
+            url = "https://ipinfo.io/api/data/asns?country=vn&amount=20&page={}"
+            headers = {
+                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0"
+                }
+            for i in range(int(amount/20)+1):
+                # sleep(randint(1, 5))
+                res = requests.get(url=url.format(i), headers=headers)
+                data = res.json()
+                if isinstance(data, list):
+                    for item in data:
+                        writer.writerow(list(item.values()))
 
-'''Lấy danh sách ASN của các ngân hàng ở Việt Nam'''
-def get_asn_bank_list():
+'''Xuất danh sách ASN của các ngân hàng ở Việt Nam ra file txt'''
+def export_asn_bank_list():
     asn_bank_list = []
     with open("DS_ASN_VN.csv", "r") as csvfile:
         reader = csv.reader(csvfile)
@@ -51,29 +77,53 @@ def get_asn_bank_list():
         for i in asn_bank_list:
             file.write(i + "\n")
 
+'''Xuất danh sách ASN của Việt Nam từ csv ra txt'''
+def export_asn_vn_list():
+    asn_list = []
+    with open("DS_ASN_VN.csv", "r") as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            if row[0]=="asn": continue
+            else:
+                asn_list.append(row[0])
+
+    with open("asn_vn_list.txt", "w") as file:
+        for i in asn_list:
+            file.write(i + "\n")
+
+'''Lấy danh sách'''
+def get_list(list_file):
+    list = []
+    with open(list_file, "r") as file:
+        lines = file.readlines()
+        for line in lines:
+            list.append(line.rstrip("\n"))
+    return list
 
 '''Lấy danh sách IP block của ASN'''
-def get_block_list():
-    asn_bank_list = get_bank_list()
+def get_ip_block_list():
+    asn_bank_list = get_list("asn_bank_vn.txt")
+    # asn_list = get_list("asn_vn_list.txt")
     for asn in asn_bank_list:
         # print(asn)
+        # sleep(randint(1, 5))
+        url = "https://ipinfo.io/{}"
+        headers = {
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0"
+            }
+        res = requests.get(url=url.format(asn), headers=headers)
+        soup = BeautifulSoup(res.text, "lxml")
+        tables = soup.find_all("table", class_="table table-bordered table-md table-details")
+        datas = tables[0].find_all("a")
         with open("Bank/{}_ip_blocks.txt".format(asn), "w") as file:
-            URL = "https://whoisrequest.com/ip/{}".format(asn)
-            # print(URL)
-            res = requests.get(url=URL, headers=headers)
-            soup = BeautifulSoup(res.text, "lxml")
-            tables = soup.find_all("table")
-            rows = tables[0].find_all("tr")
-            for row in rows:
-                datas = row.find_all("td")
-                if datas != []:
-                    # print(datas[0].text.strip())
-                    file.write(datas[0].text.strip() + "\n")
+            for data in datas:
+                file.write(data.text.strip() + "\n")
 
 '''Tạo danh sách IP từ danh sách IP Blocks'''
-def get_ip_list():
+def create_ip_list():
     ip_list = []
-    asn_bank_list = get_bank_list()
+    asn_bank_list = get_list("asn_bank_vn.txt")
+    # asn_list = get_list("asn_vn_list.txt")
     for asn in asn_bank_list:
         with open("Bank/{}_ip_blocks.txt".format(asn), "r") as fr:
             lines = fr.readlines()
@@ -89,7 +139,7 @@ def get_ip_list():
             fw.close()
         ip_list.clear()
 
-'''Tìm kiếm DNS từ danh sách IP'''
+'''Tìm kiếm DNS từ IP'''
 def reverse_dns_lookup(ip_address):
     try:
         resolver = dns.resolver.Resolver()
@@ -105,22 +155,16 @@ def reverse_dns_lookup(ip_address):
     except dns.exception.DNSException as e:
         return None
 
-def get_bank_list():
-    asn_bank_list = []
-    with open("asn_bank_vn.txt", "r") as file:
-        lines = file.readlines()
-        for line in lines:
-            asn_bank_list.append(line.rstrip("\n"))
-    return asn_bank_list
-
 def get_dns(ip: str):
     hostname = reverse_dns_lookup(ip)
     if hostname:
         # print("{}\t{}".format(ip, hostname))
         return ip, hostname
 
+'''Lấy danh sách DNS của 1 ASN'''
 def get_dns_list():
-    asn_bank_list = get_bank_list()
+    asn_bank_list = get_list("asn_bank_vn.txt")
+    # asn_list = get_list("asn_vn_list.txt")
     for asn in asn_bank_list:
         ip_list = []
         with open("Bank/IP_address/{}_ip_list.txt".format(asn), "r") as fr:
